d5cb0daae8844a4e566ccbc8cbf5f0dae056a094
Vincent Theron <vincent@ntoggle.com>
2016-04-21 15:14:06 -0400

    parse incoming request

--- lib/icepick/PlugRouter.ex
+++ lib/icepick/PlugRouter.ex
@@ -2,6 +2,9 @@ defmodule Icepick.PlugRouter do
 
   use Plug.Router
 
+  plug Plug.Parsers, parsers: [:json],
+                     pass: ["*/*"],
+                     json_decoder: Poison
   plug :match
   plug :dispatch
 
@@ -11,20 +14,21 @@ defmodule Icepick.PlugRouter do
 
   post "/supply-partners/mopub" do
     ExStatsD.increment("icepick.inbound-requests.counter")
+#    IO.inspect conn.params["_json"]
     send_resp(conn, 204, "")
     #req = Poison.Parser.parse!(body)
     #IO.puts inspect(req)
   end
 
   match _ do
-    IO.puts "Received unmatched request!"
+    ExStatsD.increment("icepick.inbound-requests.invalid")
     send_resp(conn, 204, "")
   end
 
   def start_link() do 
     {:ok, _} = Plug.Adapters.Cowboy.http(
       __MODULE__,
-      [acceptors: 1000],
+      [acceptors: 100],
       port: 8000)
   end
 
